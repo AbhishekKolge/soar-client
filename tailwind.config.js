0d7cd64b4589194cc73ddb8cfce5f6d70deb2e8f
AbhishekKolge <abhishekkolge96@gmail.com>
2024-11-18 10:43:20 +0530

    animation config added
    Signed-off-by: AbhishekKolge <abhishekkolge96@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,9 @@ export default {
           5: "hsl(var(--chart-5))",
         },
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
       },
