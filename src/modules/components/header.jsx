ba8f06af50ec0ca2873cadf0ec734a4a841ec1fa
AbhishekHybr1d <abhishek.kolge@hybr1d.io>
2024-11-30 12:52:06 +0530

    max width added for sections
    Signed-off-by: AbhishekHybr1d <abhishek.kolge@hybr1d.io>

--- src/modules/components/header.jsx
+++ src/modules/components/header.jsx
@@ -17,20 +17,22 @@ const Header = (props) => {
   };
 
   return (
-    <header className="pb-[11px] pt-[25px] px-[25px] lg:pt-[20px] lg:pb-[20px] lg:px-[40px] flex flex-col gap-[25px] bg-white">
-      <div className="flex items-center justify-between">
-        <SidebarTrigger className="lg:hidden p-0" />
-        <h2 className="text-primary text-[20px] lg:text-[28px] font-semibold leading-9">
-          {children}
-        </h2>
-        <div className="flex items-center gap-[30px]">
-          <Search className="hidden lg:block" />
-          <Settings />
-          <Notification />
-          <NavAction auth={auth} onLogout={logoutAccountHandler} />
+    <header className="pb-[11px] pt-[25px] px-[25px] lg:pt-[20px] lg:pb-[20px] lg:px-[40px] bg-white">
+      <div className="max-w-screen-xl m-auto w-full h-full flex flex-col gap-[25px]">
+        <div className="flex items-center justify-between">
+          <SidebarTrigger className="lg:hidden p-0" />
+          <h2 className="text-primary text-[20px] lg:text-[28px] font-semibold leading-9">
+            {children}
+          </h2>
+          <div className="flex items-center gap-[30px]">
+            <Search className="hidden lg:block" />
+            <Settings />
+            <Notification />
+            <NavAction auth={auth} onLogout={logoutAccountHandler} />
+          </div>
         </div>
+        <Search className="lg:hidden" />
       </div>
-      <Search className="lg:hidden" />
     </header>
   );
 };
